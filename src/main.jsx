078e7a4005ea09fde490edff8b2a9f880eef3995
yap9393 <yap_1993@hotmail.com>
2023-10-05 23:22:15 -0300

    hiding credentials

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeqcKskzTtir03WaO2EvIMkzzbp_rTYtQ",
-  authDomain: "react-coderhouse-agustin-p.firebaseapp.com",
-  projectId: "react-coderhouse-agustin-p",
-  storageBucket: "react-coderhouse-agustin-p.appspot.com",
-  messagingSenderId: "807839313626",
-  appId: "1:807839313626:web:81c65722c7eebc1ee0ffe3"
+
 };
 
 const app = initializeApp(firebaseConfig);
